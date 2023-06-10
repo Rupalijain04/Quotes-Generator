4d1b9c5a149de8839464fa5dfcbd94e8c5eecfa0
Rupali <117211594+Rupsjain@users.noreply.github.com>
2023-06-10 17:34:18 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+function fetchQuote(){
+    let randomQuote = Math.floor(Math.random()*1000)
+    fetch("https://type.fit/api/quotes")
+    .then((response) => {
+      return response.json();
+      console.log(response.json())
+    }).then((data) => {
+      console.log(data[randomQuote].text);
+      document.getElementById("quote").innerText = data[randomQuote].text;
+    });
+}
+fetchQuote();
